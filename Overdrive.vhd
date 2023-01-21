ee0d85955025a061195eff449a2c09799ef1bfc3
RaymondWFleming <122411417+RaymondWFleming@users.noreply.github.com>
2023-01-21 12:17:04 -0600

    Update Overdrive.vhd

--- Overdrive.vhd
+++ Overdrive.vhd
@@ -3,22 +3,25 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
+--Author: Raymond Fleming
+--Purpose: Soft clipping of input signal for an overdrive effect
+
 ENTITY Overdrive IS
-	Port ( Input		  : IN STD_LOGIC_VECTOR(23 downto 0);
-			 NumIn		  : IN STD_LOGIC_VECTOR(9 downto 0);
-			 Output		  : OUT STD_LOGIC_VECTOR(23 downto 0));
+	Port ( Input  : IN STD_LOGIC_VECTOR(23 downto 0);
+	       NumIn  : IN STD_LOGIC_VECTOR(9 downto 0);
+	       Output : OUT STD_LOGIC_VECTOR(23 downto 0));
 			 
 	End Overdrive;
 	
 Architecture Behavior Of Overdrive IS
-SIGNAL InInt 			  : Integer;
-SIGNAL OutInt			  : Integer;
-SIGNAL MinInt			  : Integer := -8388608;
-SIGNAL MaxInt			  : Integer := 8388607;
-SIGNAL cutoffpos       : Integer := 5000000;
-SIGNAL cutoffneg       : Integer := -5000000;
-signal maxoutpos       : Integer := 8000000;
-signal maxoutneg       : Integer := -8000000;
+SIGNAL InInt 	 : Integer;
+SIGNAL OutInt	 : Integer;
+SIGNAL MinInt	 : Integer := -8388608;
+SIGNAL MaxInt	 : Integer := 8388607;
+SIGNAL cutoffpos : Integer := 5000000;
+SIGNAL cutoffneg : Integer := -5000000;
+signal maxoutpos : Integer := 8000000;
+signal maxoutneg : Integer := -8000000;
 
 
 
@@ -28,22 +31,25 @@ BEGIN
 InInt<=to_integer(signed(Input));
 
 
+--Soft clipping effect.  If above max output, output max output.
+--If between cutoff and max, output average of input signal and cutoff.
+--If below cutoff, output input signal
 
 OutInt<= maxoutpos when InInt > maxoutpos else
 
-
-			cutoffpos+(InInt-cutoffpos)/2 when InInt > cutoffpos AND NumIn(0)='1' else
+	 cutoffpos+(InInt-cutoffpos)/2 when InInt > cutoffpos AND NumIn(0)='1' else
 			
-			InInt when InInt > 0 AND NumIn(0)='1' else
+	 InInt when InInt > 0 AND NumIn(0)='1' else
 			
-			0 when InInt = 0 AND NumIn(0)='1' else
+	 0 when InInt = 0 AND NumIn(0)='1' else
 			
-			InInt when InInt < cutoffneg AND NumIn(0)='1' else
+	 InInt when InInt < cutoffneg AND NumIn(0)='1' else
+	
+	 cutoffneg+(InInt-cutoffneg)/2 when InInt < cutoffneg AND NumIn(0)='1' else
 			
-			cutoffneg+(InInt-cutoffneg)/2 when InInt < cutoffneg AND NumIn(0)='1' else
-			
-			maxoutneg when InInt<maxoutneg AND NumIn(0)='1' else
-			InInt;
+	 maxoutneg when InInt<maxoutneg AND NumIn(0)='1' else
+		 
+	 InInt;
 
 
 Output<= std_logic_vector(to_signed(OutInt,24));
