6e5bf6b04bffce8d5ee0d6f7b6c1b011c9c54d60
RaymondWFleming <122411417+RaymondWFleming@users.noreply.github.com>
2023-01-21 12:07:56 -0600

    Update NoiseGate.vhd

--- NoiseGate.vhd
+++ NoiseGate.vhd
@@ -3,30 +3,32 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
+--Author:  Raymond Fleming
+--Purpose: Reduce noise of incoming audio signal
+
 ENTITY NoiseGate IS
-	Port ( Input		  : IN STD_LOGIC_VECTOR(23 downto 0);
+	Port ( Input  : IN STD_LOGIC_VECTOR(23 downto 0);
 
 
-			 Output		  : OUT STD_LOGIC_VECTOR(23 downto 0));
+	       Output : OUT STD_LOGIC_VECTOR(23 downto 0));
 			 
 	End NoiseGate;
 	
 Architecture Behavior Of NoiseGate IS
-SIGNAL InInt 			  : Integer;
-SIGNAL OutInt			  : Integer;
-SIGNAL MinInt			  : Integer := -8388608;
-SIGNAL MaxInt			  : Integer :=  8388607;
+SIGNAL InInt  : Integer;
+SIGNAL OutInt : Integer;
+SIGNAL MinInt : Integer := -8388608;
+SIGNAL MaxInt : Integer :=  8388607;
 
-SIGNAL GateHi			  : Integer :=  250000;
-SIGNAL GateLo			  : Integer := -25000;
-
+SIGNAL GateHi : Integer :=  25000;
+SIGNAL GateLo : Integer := -25000;
 
 
 BEGIN
 
-
 InInt<=to_integer(signed(Input));
 
+--If signal is below cutoff, output silence. Otherwise pass signal
 OutInt<=InInt when (InInt>GateHi OR InInt < GateLo) else 0;
 
 Output<= std_logic_vector(to_signed(OutInt,24));
