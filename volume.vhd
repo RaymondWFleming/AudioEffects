c07acac5ff67edecd742090795f08761e6338455
RaymondWFleming <122411417+RaymondWFleming@users.noreply.github.com>
2023-01-21 12:21:49 -0600

    Update volume.vhd

--- volume.vhd
+++ volume.vhd
@@ -3,23 +3,26 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
-ENTITY Volume IS
-	Port ( Input		  : IN STD_LOGIC_VECTOR(23 downto 0);
+--Author: Raymond Fleming
+--Purpose: Adjust volume of incoming audio signal
 
-			 NumIn		  : IN STD_LOGIC_VECTOR(9 downto 0);
-			 Output		  : OUT STD_LOGIC_VECTOR(23 downto 0));
+ENTITY Volume IS
+	Port ( Input  : IN STD_LOGIC_VECTOR(23 downto 0);
+
+	       NumIn  : IN STD_LOGIC_VECTOR(9 downto 0);
+	       Output : OUT STD_LOGIC_VECTOR(23 downto 0));
 			 
 	End Volume;
 	
 Architecture Behavior Of Volume IS
-SIGNAL InInt 			  : Integer;
-SIGNAL OutInt			  : Integer;
-SIGNAL MinInt			  : Integer := -8388608;
-SIGNAL MaxInt			  : Integer :=  8388607;
-signal volint		     : Integer;
-SIGNAL GateHi			  : Integer :=  1500000;
-SIGNAL GateLo			  : Integer := -1500000;
-signal numint			  : Integer;
+SIGNAL InInt  : Integer;
+SIGNAL OutInt : Integer;
+SIGNAL MinInt : Integer := -8388608;
+SIGNAL MaxInt : Integer :=  8388607;
+signal volint : Integer;
+SIGNAL GateHi : Integer :=  1500000;
+SIGNAL GateLo : Integer := -1500000;
+signal numint : Integer;
 
 
 BEGIN
@@ -27,6 +30,7 @@ BEGIN
 InInt<=to_integer(signed(Input));
 volint<=to_integer(unsigned(NumIn(9 downto 4)&'1'));
 
+--Take binary input from switches and output adjusted signal based on switch value
 OutInt<=InInt*volint when InInt>0 AND NumIn(3)='1' else
 		  InInt*volInt when InInt<0 AND NumIn(3)='1' else
 		  InInt when InInt = 0 AND NumIn(3)='1' else
