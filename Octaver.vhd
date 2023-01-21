10b2ae6143ca8ace977f34d9a781101d8fab9763
RaymondWFleming <122411417+RaymondWFleming@users.noreply.github.com>
2023-01-21 12:13:27 -0600

    Update Octaver.vhd

--- Octaver.vhd
+++ Octaver.vhd
@@ -3,23 +3,27 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
-ENTITY Octaver IS
-	Port ( Input		  : IN STD_LOGIC_VECTOR(23 downto 0);
-			 NumIn		  : IN STD_LOGIC_VECTOR(9 downto 0);
+--Author: Raymond Fleming
+--Purpose: Provide octave effect using full rectification of signal
+--Note: this effect creates a DC offset which will require a filtering capacitor on the output
 
-			 Output		  : OUT STD_LOGIC_VECTOR(23 downto 0));
+ENTITY Octaver IS
+	Port ( Input  : IN STD_LOGIC_VECTOR(23 downto 0);
+	       NumIn  : IN STD_LOGIC_VECTOR(9 downto 0);
+
+	       Output : OUT STD_LOGIC_VECTOR(23 downto 0));
 			 
 	End Octaver;
 	
 Architecture Behavior Of Octaver IS
-SIGNAL InInt 			  : Integer;
-SIGNAL OctInt			  : Integer;
-SIGNAL OutInt			  : Integer;
-SIGNAL MinInt			  : Integer := -8388608;
-SIGNAL MaxInt			  : Integer :=  8388607;
+SIGNAL InInt  : Integer;
+SIGNAL OctInt : Integer;
+SIGNAL OutInt : Integer;
+SIGNAL MinInt : Integer := -8388608;
+SIGNAL MaxInt : Integer :=  8388607;
 
-SIGNAL GateHi			  : Integer :=  250000;
-SIGNAL GateLo			  : Integer := -25000;
+SIGNAL GateHi : Integer :=  25000;
+SIGNAL GateLo : Integer := -25000;
 
 
 
@@ -28,12 +32,15 @@ BEGIN
 
 InInt<=to_integer(signed(Input));
 
-OctInt<=(InInt) when (InInt>0)else 
-		   0 when InInt = 0 else
-			-(InInt) when (InInt<0);
+--Output the input signal if positive, and greater than zero
+--Output zero if input is zero
+--Output absolute value of input if input is negative
+
+OctInt<=(InInt) when (InInt>0) else 
+        0 when InInt = 0 else
+	-(InInt) when (InInt<0);
 			
-
-
+--Output averaged input and octave if mising is selected with switch
 OutInt<=(OctInt+InInt)/2 when NumIn(2)='1' else InInt;
 
 Output<= std_logic_vector(to_signed(OutInt,24));
